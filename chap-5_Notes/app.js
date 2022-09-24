120e1fbfa63280c0358c72fcf32bd97f8404919f
Chima Onumaegbu <Chilex23@DESKTOP-GDGNAGI.localdomain>
2022-09-24 13:47:03 +0100

    Finished the first iteration of the notes app, Started another app using the mobile-first paradigm

--- chap-5_Notes/app.js
+++ chap-5_Notes/app.js
@@ -1,17 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const notesRouter = require('./routes/notes');
+//const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+// Register a partials folder
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/notes', notesRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
